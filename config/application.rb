509a1707f5aed8698b2340c6cf785e0f285ee7bc
Emre <emreardic1@gmail.com>
2014-08-17 00:18:05 +0300

    Cache file store in public

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module DiyanetAPI
 
     config.assets.initialize_on_precompile = false
 
+    config.cache_store = :file_store, Rails.public_path
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
